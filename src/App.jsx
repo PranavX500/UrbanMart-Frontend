c0fdfde02818f1102b5b7254c0c2bcbd05e640b5
PranavX500 <ps5840432@gmail.com>
2025-12-17 12:25:54 +0530

    React-app

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Otp from './components/Otp'
+import ProductPage from './components/ProductPage'
+import ProductDetail from './components/ProductDetail'
+import Dashboard from "./components/Dashboard";
+import OrderSummary from './components/OrderSummary'
+import CartPage from "./components/CartPage";  
+import OrdersPage from './components/OrdersPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-shell">
+        <Dashboard/>
+        <Routes>
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/otp" element={<Otp />} />
+          <Route path="/" element={<ProductPage />} />
+          <Route path="/order-summary" element={<OrderSummary />} />
+           <Route path="/orders" element={<OrdersPage />} />
+
+
+
+          <Route path="/cart" element={<CartPage />} />
+
+        
+          <Route path="*" element={<Navigate to="/" replace />} />
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
